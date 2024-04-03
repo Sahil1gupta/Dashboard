b48d1be14d5429dbed1f08db0158c0aadd40af0c
Sahil1gupta <ej1905630411@gmail.com>
2024-04-03 12:37:31 +0530

    tag changed

--- src/components/MyComponents/dashboard/SidebarMobile.jsx
+++ src/components/MyComponents/dashboard/SidebarMobile.jsx
@@ -55,7 +55,7 @@ function SidebarMobile() {
             <span className="sr-only">Acme Inc</span>
           </Link>
           <Link
-            to="#"
+            to=""
             className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
           >
             <Home className="h-5 w-5" />
